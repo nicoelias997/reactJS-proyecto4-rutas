46d289a3c41d91e54e983d69b838f6c18b22bf79
nicoelias997 <nicoelias997@gmail.com>
2022-09-04 20:41:52 +0200

    Agrego una fc async await para consumir una API, creo una const con esos dato de la api, y la genero en setEquipo, luego hago un recorrido y la pinto en el div

--- src/components/Nosotros.jsx
+++ src/components/Nosotros.jsx
@@ -2,13 +2,32 @@ import React from 'react'
 
 const Nosotros = () => {
 
+ 
+
+  const [equipo, setEquipo] = React.useState([])
+
   React.useEffect(()=>{
-    console.log("Efecto")
-  })
+    obtenerDatos()
+  }, [])
+
+  const obtenerDatos = async () => {
+   const data =  await fetch("https://jsonplaceholder.typicode.com/users")
+   const users = await data.json()
+   setEquipo(users)
+  }
 
   return (
     <div>
         <h1>Nostros</h1>
+        <ul>
+          {
+            equipo.map(item => (
+              <li key={item.id}>
+                {item.name} & {item.email}
+              </li>
+            ))
+          }
+        </ul>
     </div>
   )
 }
